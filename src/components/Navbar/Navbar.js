122b7e2059e31d0defd796256f515d691897d9e1
antmensch <105740241+antmensch@users.noreply.github.com>
2022-07-22 13:04:50 +0300

    JSX done, basic CSS done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css'
 import logo from '../../img/logo.svg'
+import dashboardIcon from './img/dashboard.svg'
+import expensesIcon from './img/expenses.svg'
+import settingsIcon from './img/settings.svg'
 
 
-function Navbar() {
+export default function Navbar() {
+
+  function handleLogout() {
+    console.log('log out button clicked')
+  }
+
   return (
     <nav className='navbar'>
-      <Link to='/'> 
-      <img src={logo} alt="website logo" width="30px" height="30px" className='navbar--logo'/> 
-      <span className='navbar--logoText'> Maglo. </span>
-      </Link>
+      <div className="navbar--logoWrapper">
+        <Link to='/'> 
+        <img src={logo} alt="website logo" width="30px" height="30px" className='navbar--logo'/> 
+        <span className='navbar--logoText'> Maglo. </span>
+        </Link>
+      </div> 
+      
       <ul className='navbar--menu'>
         <li className='navbar--menuItem'>
           <NavLink to="/dashboard">
@@ -27,12 +39,13 @@ function Navbar() {
             Settings
           </NavLink>
         </li>
-
       </ul>
+
+      <div className="nav--buttonWrapper">
+        <button onClick={handleLogout}>Log out</button>
+      </div>
       
 
     </nav>
   )
 }
-
-export default Navbar;
