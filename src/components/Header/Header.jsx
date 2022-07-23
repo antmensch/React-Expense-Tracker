0d9a4398ef3d992e06caa458fca450f8e6f9a769
antmensch <105740241+antmensch@users.noreply.github.com>
2022-07-23 23:27:36 +0300

    continue working on user menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
+import userpic from './img/userpic.png'
+import arrow from './img/arrow.svg'
+import { useState } from 'react'
 import './Header.css'
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        setMenuOpen(prev => !prev);
+    }
   return (
     <header className='header'>
         <div className='header--currentComponentName'>Dashboard</div>
         <div className="header--menu">
-            <img className='header--menuImage' width="36px" height="36px" src='' alt=''/>
-            <button className='header--menuButton'>
-                <span>User Name</span>
-                <img src="" alt="" />
+            <img className='header--menuImage' width="36px" height="36px" src={userpic} alt='user picture'/>
+            <button className='header--menuButton' onClick={toggleMenu}>
+                <span className='header--buttonText'>Mahfuzul Nabil</span>
+                <img className={"header--buttonImage" + (menuOpen ? " open" : "")} src={arrow} alt="arrow" />
             </button>
-            <div></div>
+            <div className='header--dropdown'>
+                <ul className='header--dropdownList'>
+                    <li className='header--dropdownListItem'>Item 1</li>
+                    <li className='header--dropdownListItem'>Item 2</li>
+                    <li className='header--dropdownListItem'>Item 3</li>
+                </ul>
+            </div>
         </div>
     </header>
   )
